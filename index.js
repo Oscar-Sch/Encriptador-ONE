3445ad9a9784d8c2b74b38f9920f911258d42fbb
Oscar <oscarschwerdt@gmail.com>
2022-08-13 19:06:03 -0300

    agregado el candado y otros efectos esteticos

--- index.js
+++ index.js
@@ -3,13 +3,30 @@ const textoutput=document.getElementById("msg-output");
 const button=document.getElementById("but");
 const button2=document.getElementById("but2");
 const buttonCopy=document.getElementById("butcopy");
+const root=document.querySelector(":root");
 
+function transferAnimOn(){
+    root.style.setProperty("--rotate","rotate ease 1.5s infinite");
+}
+function transferAnimOff(){
+    root.style.setProperty("--rotate","none");
+}
+function padlockAnimOff(){
+    root.style.setProperty("--padlock","0");
+}
+function padlockAnimOn(){
+    root.style.setProperty("--padlock","1");
+}
 
-
-function inputAnimation() {
+function inputAnimation(msg) {
     let input=textarea.value;
-    setInterval(() => {
-        if (input==""){return};
+    transferAnimOn();
+    if(msg!=="")padlockAnimOn();
+    let inputInterval=setInterval(() => {
+        if (input.length==0){
+            outputAnimation(msg);
+            clearInterval(inputInterval);
+            return};
         input=input.slice(1,input.length -1);
         textarea.value=input;
     }, 60);
@@ -19,8 +36,13 @@ function inputAnimation() {
 function outputAnimation(msg) {
     let cont=1;
     let output="";
-    setInterval(() => {
-        if (cont>msg.length){return};
+    let outputInterval=setInterval(() => {
+        if (cont>msg.length){
+            transferAnimOff();
+            padlockAnimOff();
+            clearInterval(outputInterval);
+            return
+        };
         output=msg.slice(msg.length-cont,msg.length);
         textoutput.value=output;
         cont++;
@@ -29,6 +51,7 @@ function outputAnimation(msg) {
 }
 
 function encrypt(){
+    textoutput.value="";
     const input=textarea.value;
     let output="";
     for (char of input){
@@ -55,13 +78,14 @@ function encrypt(){
         }
     }
 
-    inputAnimation();
-    outputAnimation(output);
+    inputAnimation(output);
+    
 
     // return output;
 }
 
 function decrypt() {
+    textoutput.value="";
     const input=textarea.value;
     let output="";
 
@@ -71,19 +95,20 @@ function decrypt() {
     output= output.replace(/ober/gi,"o");
     output= output.replace(/ufat/gi,"u");
 
-    inputAnimation();
-    outputAnimation(output);
-
+    inputAnimation(output);
 }
 
 
 button.addEventListener("click", ()=>{
+    root.style.setProperty("--anim-color","255, 36, 0");
+    root.style.setProperty("--padlock-bg","linear-gradient(0deg, rgb(75, 18, 2) 0%, rgb(108, 61, 3) 35%, rgb(195, 62, 5) 100%)");
     encrypt();
     // textoutput.value=encrypt();
     // textoutput.value=inputAnimation();
 });
 button2.addEventListener("click", ()=>{
-    
+    root.style.setProperty("--anim-color","36, 255, 0");
+    root.style.setProperty("--padlock-bg","linear-gradient(0deg, rgba(4,75,2,1) 0%, rgba(3,108,17,1) 35%, rgba(89,195,5,1) 100%)");
     decrypt();
     // textoutput.value=decrypt();
 });
