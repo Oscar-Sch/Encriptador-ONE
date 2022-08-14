6c84851e16e71caee67777a95a5a8257c4138bd8
Oscar <oscarschwerdt@gmail.com>
2022-08-14 02:31:52 -0300

    nuevos efectos añadidos

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const textarea=document.getElementById("msg-input");
 const textoutput=document.getElementById("msg-output");
 const button=document.getElementById("but");
 const button2=document.getElementById("but2");
+const buttons=document.getElementsByClassName("button");
 const buttonCopy=document.getElementById("butcopy");
 const root=document.querySelector(":root");
 
@@ -17,10 +18,41 @@ function padlockAnimOff(){
 function padlockAnimOn(){
     root.style.setProperty("--padlock","1");
 }
+function buttonsDisable(){
+    button.disabled=true;
+    button2.disabled=true;
+    buttonCopy.disabled=true;
+    button.style.setProperty("cursor","default");
+    button2.style.setProperty("cursor","default");
+    buttonCopy.style.setProperty("cursor","default");
+    setTimeout(() => {
+        root.style.setProperty("--but-hover-color","var(--green)");
+        root.style.setProperty("--but-hover-border","var(--green)");
+        root.style.setProperty("--but-hover-bshadow","none");
+        root.style.setProperty("--but-hover-tshadow","none");
+        
+    }, 500);
+}
+function buttonsEnable(){
+    button.disabled=false;
+    button2.disabled=false;
+    buttonCopy.disabled=false;
+    button.style.setProperty("cursor","pointer");
+    button2.style.setProperty("cursor","pointer");
+    buttonCopy.style.setProperty("cursor","pointer");
+    setTimeout(() => {
+        root.style.setProperty("--but-hover-color","rgb(146, 255, 146)");
+        root.style.setProperty("--but-hover-border","rgb(146, 255, 146)");
+        root.style.setProperty("--but-hover-bshadow"," 0 0 5px 1px var(--green)");
+        root.style.setProperty("--but-hover-tshadow"," -1px 0 var(--green), 0 1px var(--green), 1px 0 var(--green), 0 -1px var(--green)");
+        
+    }, 500);
+}
 
 function inputAnimation(msg) {
     let input=textarea.value;
     transferAnimOn();
+    buttonsDisable();
     if(msg!=="")padlockAnimOn();
     let inputInterval=setInterval(() => {
         if (input.length==0){
@@ -40,6 +72,10 @@ function outputAnimation(msg) {
         if (cont>msg.length){
             transferAnimOff();
             padlockAnimOff();
+            setTimeout(() => {
+                root.style.setProperty("--padlock-anim","none");
+            }, 1000);
+            buttonsEnable();
             clearInterval(outputInterval);
             return
         };
@@ -101,6 +137,7 @@ function decrypt() {
 
 button.addEventListener("click", ()=>{
     root.style.setProperty("--anim-color","255, 36, 0");
+    root.style.setProperty("--padlock-anim","close");
     root.style.setProperty("--padlock-bg","linear-gradient(0deg, rgb(75, 18, 2) 0%, rgb(108, 61, 3) 35%, rgb(195, 62, 5) 100%)");
     encrypt();
     // textoutput.value=encrypt();
@@ -108,6 +145,7 @@ button.addEventListener("click", ()=>{
 });
 button2.addEventListener("click", ()=>{
     root.style.setProperty("--anim-color","36, 255, 0");
+    root.style.setProperty("--padlock-anim","open");
     root.style.setProperty("--padlock-bg","linear-gradient(0deg, rgba(4,75,2,1) 0%, rgba(3,108,17,1) 35%, rgba(89,195,5,1) 100%)");
     decrypt();
     // textoutput.value=decrypt();
